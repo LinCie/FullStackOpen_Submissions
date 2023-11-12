fc470b8adf302614f04304d36d4b672d056a1b4d
LinCie <aldiofernanda@protonmail.com>
2023-11-12 13:54:15 +0700

    exercise 1.14 submission

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,5 +1,38 @@
 import { useState } from "react";
 
+const Button = (props) => {
+  const { text, onClick } = props;
+  return <button onClick={onClick}>{text}</button>;
+};
+
+const Anecdote = (props) => {
+  const { anecdote, votes } = props;
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <p>{anecdote}</p>
+      <p>Votes {votes}</p>
+    </div>
+  );
+};
+
+const BestAnecdotes = (props) => {
+  const { total, anecdote, votes } = props;
+  return (
+    <div>
+      <h1>Anecdote with most votes</h1>
+      {total ? (
+        <>
+          <p>{anecdote}</p>
+          <p>Votes {votes}</p>
+        </>
+      ) : (
+        <p>There is no vote</p>
+      )}
+    </div>
+  );
+};
+
 const App = () => {
   const anecdotes = [
     "If it hurts, do it more often.",
@@ -16,20 +49,33 @@ const App = () => {
 
   const [selected, setSelected] = useState(randomAnecdote());
   const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
+  const [totalVotes, setTotalVotes] = useState(0);
+  const [bestAnecdote, setBestAnecdote] = useState({
+    index: 0,
+    votes: 0,
+  });
 
   const randomizeSelected = () => setSelected(randomAnecdote());
   const addVote = () => {
     const copy = [...votes];
     copy[selected] += 1;
     setVotes(copy);
+    if (copy[selected] > bestAnecdote.votes) {
+      setBestAnecdote({ index: selected, votes: copy[selected] });
+    }
+    setTotalVotes(totalVotes + 1);
   };
 
   return (
     <div>
-      <p>{anecdotes[selected]}</p>
-      <p>Votes {votes[selected]}</p>
-      <button onClick={addVote}>Vote</button>
-      <button onClick={randomizeSelected}>Next Anecdote</button>
+      <Anecdote anecdote={anecdotes[selected]} votes={votes[selected]} />
+      <Button onClick={addVote} text="Vote" />
+      <Button onClick={randomizeSelected} text="Next Anecdote" />
+      <BestAnecdotes
+        total={totalVotes}
+        anecdote={anecdotes[bestAnecdote.index]}
+        votes={bestAnecdote.votes}
+      />
     </div>
   );
 };
