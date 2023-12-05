ef874908fddd45b4662645a456f2757c880b3477
LinCie <aldiofernanda@protonmail.com>
2023-12-05 16:34:38 +0700

    exercise 2.1 submission

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,27 +1,52 @@
+const CourseItem = (props) => {
+  const { courseItem } = props;
+
+  return (
+    <li>
+      {courseItem.name} {courseItem.exercises}
+    </li>
+  );
+};
+
+const Course = (props) => {
+  const { course } = props;
+
+  return (
+    <div>
+      <h1>{course.name}</h1>
+      <ul>
+        {course.parts.map((courseItem) => (
+          <CourseItem key={courseItem.id} courseItem={courseItem} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
 const App = () => {
   const course = {
     id: 1,
-    name: 'Half Stack application development',
+    name: "Half Stack application development",
     parts: [
       {
-        name: 'Fundamentals of React',
+        name: "Fundamentals of React",
         exercises: 10,
-        id: 1
+        id: 1,
       },
       {
-        name: 'Using props to pass data',
+        name: "Using props to pass data",
         exercises: 7,
-        id: 2
+        id: 2,
       },
       {
-        name: 'State of a component',
+        name: "State of a component",
         exercises: 14,
-        id: 3
-      }
-    ]
-  }
+        id: 3,
+      },
+    ],
+  };
 
-  return <Course course={course} />
-}
+  return <Course course={course} />;
+};
 
-export default App
+export default App;
