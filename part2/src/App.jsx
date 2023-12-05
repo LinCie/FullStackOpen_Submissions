0111615522fa2f7943985c8e9d9489c32fa6d42e
LinCie <aldiofernanda@protonmail.com>
2023-12-05 17:21:05 +0700

    exercise 2.5 submission

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,37 +1,4 @@
-const ExerciseSum = (props) => {
-  const { parts } = props;
-  const sum = parts.reduce((a, b) => {
-    return a + b.exercises;
-  }, 0);
-
-  return <li style={{ fontWeight: "700" }}>Total of {sum} exercises</li>;
-};
-
-const CourseItem = (props) => {
-  const { courseItem } = props;
-
-  return (
-    <li>
-      {courseItem.name} {courseItem.exercises}
-    </li>
-  );
-};
-
-const Course = (props) => {
-  const { course } = props;
-
-  return (
-    <div>
-      <h1>{course.name}</h1>
-      <ul>
-        {course.parts.map((courseItem) => (
-          <CourseItem key={courseItem.id} courseItem={courseItem} />
-        ))}
-        <ExerciseSum parts={course.parts} />
-      </ul>
-    </div>
-  );
-};
+import Course from "./components/Course"
 
 const App = () => {
   const courses = [
