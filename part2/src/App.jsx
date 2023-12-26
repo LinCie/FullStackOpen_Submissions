9b6e918b50143b25cb33e1d723507f8add29abdb
LinCie <aldiofernanda@protonmail.com>
2023-12-26 15:22:48 +0700

    exercise 2.9 submission

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -3,9 +3,13 @@ import _ from "lodash";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "123-456-789" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [inputValue, setInputValue] = useState({ name: "", number: "" });
+  const [search, setSearch] = useState("");
 
   const handleInputChange = (e) => {
     setInputValue({
@@ -16,13 +20,17 @@ const App = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    const newPerson = {
+      ...inputValue,
+      id: [...persons].pop().id + 1,
+    };
     for (const person of persons) {
-      if (_.isEqual(inputValue, person)) {
-        alert(`${inputValue.name} is already added to phonebook`);
+      if (_.isEqual(newPerson, person)) {
+        alert(`${newPerson.name} is already added to phonebook`);
         return;
       }
     }
-    setPersons(persons.concat(inputValue));
+    setPersons(persons.concat(newPerson));
     setInputValue({
       name: "",
       number: "",
@@ -32,9 +40,14 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Search For:{" "}
+        <input value={search} onChange={(e) => setSearch(e.target.value)} />
+      </div>
+      <h3>Add New</h3>
       <form onSubmit={handleSubmit}>
         <div>
-          name:{" "}
+          Name:{" "}
           <input
             name="name"
             value={inputValue.name}
@@ -42,7 +55,7 @@ const App = () => {
           />
         </div>
         <div>
-          number:{" "}
+          Number:{" "}
           <input
             name="number"
             value={inputValue.number}
@@ -54,11 +67,17 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <div key={person.name}>
-          {person.name} {person.number}
-        </div>
-      ))}
+      {persons.map((person) => {
+        const testName = person.name.toLowerCase();
+        if (!testName.includes(search.toLowerCase())) {
+          return null;
+        }
+        return (
+          <div key={person.id}>
+            {person.name} {person.number}
+          </div>
+        );
+      })}
     </div>
   );
 };
