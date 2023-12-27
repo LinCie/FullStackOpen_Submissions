dad4dae93ac538568d636a9b8834261fa9854e4c
LinCie <aldiofernanda@protonmail.com>
2023-12-27 17:25:01 +0700

    exercise 2.14 submission

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, createContext, useContext } from "react";
 import _ from "lodash";
-import { getNumbers, addNumber } from "./services/numbers";
+import { getNumbers, addNumber, deleteNumber } from "./services/numbers";
 
 const Search = ({ handleSearch, value }) => {
   return (
@@ -36,7 +36,33 @@ const Form = ({ handleSubmit, handleInputChange, inputValue }) => {
   );
 };
 
-const Numbers = ({ persons, search }) => {
+const DeleteButton = ({ id, name }) => {
+  const persons = useContext(PersonsContext);
+  const setPersons = useContext(SetPersonsContext);
+
+  const handleClick = async () => {
+    if (
+      window.confirm(
+        `Are you sure you want to delete ${name} from the database?`
+      )
+    ) {
+      try {
+        await deleteNumber(id);
+
+        const newPersons = persons.filter((p) => p.id !== id);
+        setPersons(newPersons);
+      } catch (err) {
+        alert(`Error when deleting ${name} from the database`);
+      }
+    }
+  };
+
+  return <button onClick={handleClick}>Delete</button>;
+};
+
+const Numbers = ({ search }) => {
+  const persons = useContext(PersonsContext);
+
   return persons.map((person) => {
     const testName = person.name.toLowerCase();
     if (!testName.includes(search.toLowerCase())) {
@@ -45,11 +71,15 @@ const Numbers = ({ persons, search }) => {
 
     return (
       <div key={person.id}>
-        {person.name} {person.number}
+        {person.name} {person.number}{" "}
+        <DeleteButton id={person.id} name={person.name} />
       </div>
     );
   });
 };
+
+const PersonsContext = createContext();
+const SetPersonsContext = createContext();
 
 const App = () => {
   const [persons, setPersons] = useState([]);
@@ -111,16 +141,20 @@ const App = () => {
 
   return (
     <div>
-      <h2>Phonebook</h2>
-      <Search handleSearch={handleSearch} value={search} />
-      <h3>Add New</h3>
-      <Form
-        handleInputChange={handleInputChange}
-        handleSubmit={handleSubmit}
-        inputValue={inputValue}
-      />
-      <h2>Numbers</h2>
-      <Numbers search={search} persons={persons} />
+      <PersonsContext.Provider value={persons}>
+        <SetPersonsContext.Provider value={setPersons}>
+          <h2>Phonebook</h2>
+          <Search handleSearch={handleSearch} value={search} />
+          <h3>Add New</h3>
+          <Form
+            handleInputChange={handleInputChange}
+            handleSubmit={handleSubmit}
+            inputValue={inputValue}
+          />
+          <h2>Numbers</h2>
+          <Numbers search={search} />
+        </SetPersonsContext.Provider>
+      </PersonsContext.Provider>
     </div>
   );
 };
--- part2/src/services/numbers.js
+++ part2/src/services/numbers.js
@@ -7,9 +7,12 @@ export const getNumbers = async () => {
     const response = await axios.get(`${baseUrl}/persons`);
     if (response.data) {
       return response.data;
+    } else {
+      throw new Error("Error getting numbers from the database");
     }
   } catch (err) {
     console.log(err);
+    throw err;
   }
 };
 
@@ -18,8 +21,25 @@ export const addNumber = async (number) => {
     const response = await axios.post(`${baseUrl}/persons`, number);
     if (response.data) {
       return response.data;
+    } else {
+      throw new Error("Error adding new number into the database");
     }
   } catch (err) {
     console.log(err);
+    throw err;
+  }
+};
+
+export const deleteNumber = async (id) => {
+  try {
+    const response = await axios.delete(`${baseUrl}/persons/${id}`);
+    if (response.data) {
+      return response.data;
+    } else {
+      throw new Error("Error deleting the number from the database");
+    }
+  } catch (err) {
+    console.log(err);
+    throw err;
   }
 };
